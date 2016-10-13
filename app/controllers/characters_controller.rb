b09718c8beafd713e965687a1d2c50391fd1625e
Nick Oki <nick.oki@live.com>
2016-10-13 11:56:00 -0400

    update characters_controller.rb

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,50 @@
+class CharactersController < ApplicationController
+
+  # def index
+  #   @characters = Character.all
+  # end
+
+  def show
+    @house = House.find params[:house_id]
+    @character = Character.find params[:id]
+  end
+
+  def new
+    @house = House.find params[:house_id]
+    @character = Character.new
+  end
+
+  def create
+    @house = House.find params[:house_id]
+    @character = @house.characters.create!(character_params)
+
+    redirect_to house_path(@house)
+  end
+
+  def edit
+    @house = House.find params[:house_id]
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @house = House.find params[:house_id]
+    @character = Character.find params[:id]
+    @character.update(character_params)
+
+    redirect_to house_character_path(@house, @character)
+  end
+
+  def destroy
+    @house = House.find params[:house_id]
+    @character = Character.find params[:id]
+    @character.destroy
+
+    redirect_to house_path(@house)
+  end
+
+  # strong params
+  private
+  def character_params
+    params.require(:character).permit(:first_name, :last_name, :alive)
+  end
+end
