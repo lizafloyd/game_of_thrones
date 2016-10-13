ffbf2f90b35021208fc6a231f76756aeca086e09
Nick Oki <nick.oki@live.com>
2016-10-13 10:09:40 -0400

    update houses_controller.rb

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,22 @@
+class HousesController < ApplicationController
+
+  #index
+  def index
+    @houses = House.all
+  end
+
+  #show
+
+  #new
+
+  #create
+
+  #edit
+
+  #update
+
+  #destroy
+
+  #private
+
+end
